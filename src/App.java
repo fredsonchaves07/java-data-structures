852cba3361de62c21325ddfdb48d4d58514c4f67
Fredson Chaves <fredsonchaves07@gmail.com>
2021-12-27 19:34:24 -0300

    feat: add stack implementation

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
+import datastructures.Stack;
+
 public class App {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args){
+        /* Testes com pilha */
+        runStack();
+
+    }
+
+    public static void runStack(){
+        Stack<Integer> stack = new Stack<Integer>();
+
+        System.out.println(stack.isEmpty());
+        stack.push(5);
+        stack.push(8);
+        System.out.println(stack.isEmpty());
+        System.out.println(stack.peek());
+        stack.push(11);
+        System.out.println(stack.peek());
+        System.out.println(stack.size());
+        stack.pop();
+        System.out.println(stack.peek());
+        System.out.println(stack.toString());
     }
 }
---
+++ src/datastructures/Stack.java
@@ -0,0 +1,42 @@
+package datastructures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Stack <T>{
+    
+    private List<T> items;
+
+    public Stack(){
+        this.items = new ArrayList<>();
+    }
+
+    public void push(T element){
+        this.items.add(element);
+    }
+
+    public T pop(){
+        return this.items.remove(this.items.size() - 1);
+    }
+
+    public T peek(){
+        return this.items.get(this.items.size() - 1);
+    }
+
+    public boolean isEmpty(){
+        return this.items.size() == 0;
+    }
+
+    public int size(){
+        return this.items.size();
+    }
+
+    public void clear(){
+        this.items.clear();
+    }
+
+    @Override
+    public String toString() {
+        return this.items.toString();
+    }
+}
