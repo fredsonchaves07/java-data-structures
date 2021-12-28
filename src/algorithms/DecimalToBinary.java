521ef234459484fbe5296f3e8d171974b0c34056
Fredson Chaves <fredsonchaves07@gmail.com>
2021-12-28 17:26:16 -0300

    feat: add decimal to binary algorithm

---
+++ src/algorithms/DecimalToBinary.java
@@ -0,0 +1,30 @@
+package algorithms;
+
+import datastructures.Stack;
+
+public class DecimalToBinary {
+
+    public static int decimalToBinary(int number){
+        Stack<Integer> stack = new Stack<>();
+        String binaryString = "";
+
+        while(number > 0){
+            int rem = number % 2;
+            number = number / 2;
+            stack.push(rem);
+        }
+
+        while(!stack.isEmpty()){
+            binaryString += stack.pop();
+        }
+
+        return Integer.parseInt(binaryString);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(decimalToBinary(233));
+        System.out.println(decimalToBinary(10));
+        System.out.println(decimalToBinary(1000));
+    }
+
+}
