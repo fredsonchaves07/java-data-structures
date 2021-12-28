e1466fd5e101d70424ce60241f6f3ec388d59515
Fredson Chaves <fredsonchaves07@gmail.com>
2021-12-28 18:04:58 -0300

    feat: add palindrome algorithm

---
+++ src/algorithms/Palindrome.java
@@ -0,0 +1,38 @@
+package algorithms;
+
+import datastructures.Stack;
+
+public class Palindrome {
+    
+    public static boolean isPalindrome(String string){
+        Stack<String> stack = new Stack<>();
+        String[] charString = string.replaceAll(" ", "").toLowerCase().split("");
+        String stringReverse = "";
+
+        if(string == ""){
+            return false;
+        }
+
+        for(int indexChar = 0; indexChar < charString.length; indexChar ++){
+            stack.push(charString[indexChar]);
+        }
+
+        while(!stack.isEmpty()){
+            stringReverse += stack.pop();
+        }
+
+        if(!stringReverse.equals(string.replaceAll(" ", "").toLowerCase())){
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome("Ana"));
+        System.out.println(isPalindrome("Jhon"));
+        System.out.println(isPalindrome("A babá baba"));
+        System.out.println(isPalindrome("Socorram me subi no onibus em Marrocos"));
+        System.out.println(isPalindrome("O cachorro late"));
+    }
+}
