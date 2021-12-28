8b92a655596f5d2ead537b63a4925cb076cd4231
Fredson Chaves <fredsonchaves07@gmail.com>
2021-12-28 17:42:24 -0300

    feat: base converter algorithm

---
+++ src/algorithms/BaseConverter.java
@@ -0,0 +1,29 @@
+package algorithms;
+
+import datastructures.Stack;
+
+public class BaseConverter {
+
+    public static String baseConverter(int number, int base){
+        Stack<Integer> stack = new Stack<>();
+        String baseConverterString = "";
+        String digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+        while(number > 0){
+            int rem = number % base;
+            number = number / base;
+            stack.push(rem);
+        }
+
+        while(!stack.isEmpty()){
+            baseConverterString += digits.charAt(stack.pop());
+        }
+
+        return baseConverterString;
+    }
+    public static void main(String[] args) {
+        System.out.println(baseConverter(100345, 2));
+        System.out.println(baseConverter(100345, 8));
+        System.out.println(baseConverter(100345, 16));
+    }
+}
