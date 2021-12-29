e467c64b67ed58df5343b0a801a8046a8d9ecc2f
Fredson Chaves <fredsonchaves07@gmail.com>
2021-12-29 15:11:47 -0300

    feat queue datastructures implementation

---
+++ src/datastructures/Queue.java
@@ -0,0 +1,42 @@
+package datastructures;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Queue<T> {
+    
+    private List<T> items;
+
+    public Queue(){
+        this.items = new ArrayList<>();
+    }
+
+    public void enqueue(T element){
+        this.items.add(element);
+    }
+
+    public T dequeue(){
+        return this.items.remove(0);
+    }
+
+    public T peek(){
+        return this.items.get(0);
+    }
+
+    public boolean isEmpty(){
+        return this.items.size() == 0;
+    }
+
+    public int size(){
+        return this.items.size();
+    }
+
+    public void clear(){
+        this.items.clear();
+    }
+
+    @Override
+    public String toString() {
+        return this.items.toString();
+    }
+}
