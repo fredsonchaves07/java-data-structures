32872a130058c17845b0fee03f8e20c953afe6c9
Fredson Chaves <fredsonchaves07@gmail.com>
2022-01-03 19:50:02 -0300

    feat: add hotPotato game

---
+++ src/algorithms/HotPotato.java
@@ -0,0 +1,30 @@
+package algorithms;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import datastructures.Queue;
+
+public class HotPotato {
+    
+    public static String hotPotato(List<String> elementsList, int quantity){
+        Queue<String> queue = new Queue<>();
+        List<String> eliminatedList = new ArrayList<>();
+        for(String element : elementsList){
+            queue.enqueue(element);
+        }
+        while(queue.size() > 1){
+            for(int i = 0; i < quantity; i ++){
+                queue.enqueue(queue.dequeue());
+            }
+            eliminatedList.add(queue.dequeue());
+        }
+        return "eliminated: " + eliminatedList + "\nwinner: " + queue.dequeue(); 
+    }
+
+    public static void main(String[] args) {
+        String[] names = {"Jhon", "Jack", "Camila", "Ingrid", "Carl"};
+        System.out.println(hotPotato(Arrays.asList(names) , 7));
+    }
+}
