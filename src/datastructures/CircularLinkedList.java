c1fec1c2c1d2b44a6c9e0aaa4bf496f84c944440
Fredson Chaves <fredsonchaves07@gmail.com>
2022-01-19 17:03:05 -0300

    feat: add circularList

---
+++ src/datastructures/CircularLinkedList.java
@@ -0,0 +1,57 @@
+package datastructures;
+
+import models.Node;
+
+public class CircularLinkedList <T> extends LinkedList <T>{
+
+    public CircularLinkedList() {
+        super();
+    }
+
+    @Override
+    public void insert(T element, int index) {
+        if (index >= 0 && index <= this.count) {
+            Node node = new Node(element);
+            Node nodeCurrent = this.head;
+            if (index == 0) {
+                if (this.head == null) {
+                    this.head = node;
+                    node.setNext(this.head);
+                } else {
+                    node.setNext(nodeCurrent);
+                    nodeCurrent = this.getElementAt(this.size());
+                    this.head = node;
+                    nodeCurrent.setNext(this.head);
+                }
+            } else {
+                Node previous = this.getElementAt(index - 1);
+                node.setNext(previous.getNext());
+                previous.setNext(node);
+            }
+            this.count ++;
+        }
+    }
+
+    @Override
+    public void removeAt(int index) {
+        if (index >= 0 && index < this.count) {
+            Node nodeCurrent = this.head;
+            if (index == 0) {
+                if (this.size() == 1) {
+                    this.head = null;
+                } else {
+                    Node removed = this.head;
+                    nodeCurrent = this.getElementAt(this.size());
+                    this.head = this.head.getNext();
+                    nodeCurrent.setNext(this.head);
+                    nodeCurrent = removed;
+                }
+            } else {
+                Node previous = this.getElementAt(index - 1);
+                nodeCurrent = previous.getNext();
+                previous.setNext(nodeCurrent.getNext());
+            }
+            this.count --
+        }
+    }
+}
