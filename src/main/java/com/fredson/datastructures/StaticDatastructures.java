2e171f30dcfa28937a7e83f47ce6c4642665c821
Fredson Chaves <fredsonchaves07@gmail.com>
2022-02-26 17:01:53 -0300

    feat: StaticDatastructures implementation

---
+++ src/main/java/com/fredson/datastructures/StaticDatastructures.java
@@ -0,0 +1,100 @@
+package com.fredson.datastructures;
+
+public class StaticDatastructures <T>{
+
+    protected T[] elements;
+    protected Integer length;
+
+    public StaticDatastructures(int capacity) {
+        this.elements = (T[]) new Object[capacity];
+        length = 0;
+    }
+
+    public StaticDatastructures() {
+        this(10);
+    }
+
+    protected boolean push(T element){
+        if (isFull()) increaseCapacity();
+        this.elements[length] = element;
+        length ++;
+        return  true;
+    }
+
+    protected boolean push(T element, int position) {
+        if (isFull()) increaseCapacity();
+        try {
+            if (length + 1 - position >= 0)
+                System.arraycopy(elements, position, elements, position + 1, length + 1 - position);
+            elements[position] = element;
+            length ++;
+        } catch (ArrayIndexOutOfBoundsException error){
+            return false;
+        }
+        return true;
+    }
+
+    protected T getElement(int position) {
+        try {
+            return elements[position];
+        } catch (ArrayIndexOutOfBoundsException error) {
+            return null;
+        }
+    }
+
+    protected boolean remove(int position) {
+        try {
+            for (int i = position; i <= length - 1; i ++) {
+                elements[i] = elements[i + 1];
+            }
+            length --;
+        } catch (ArrayIndexOutOfBoundsException error) {
+            return false;
+        }
+        return true;
+    }
+
+    protected boolean remove(T element) {
+        for (int i = 0; i <= length; i ++) {
+            if (element.equals(getElement(i))) {
+                remove(i);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    protected int lastIndexOf(T element) {
+        for (int i = length - 1; i >= 0; i --) {
+            if (elements[i].equals(element)) return i;
+        }
+        return - 1;
+    }
+
+    public boolean contain(T element) {
+        for (Object elementVector : elements) {
+            if (elementVector != null && elementVector.equals(element)) return true;
+        }
+        return false;
+    }
+
+    public int length() {
+        return length;
+    }
+
+    public boolean isEmpty() {
+        return length == 0;
+    }
+
+    private boolean isFull() { return length >= elements.length; }
+
+    private void increaseCapacity() {
+        if (length == elements.length) {
+            T[] newElements = (T[]) new Object[this.elements.length * 2];
+            for (int i = 0; i < length; i ++) {
+                newElements[i] = elements[i];
+            }
+            elements = newElements;
+        }
+    }
+}
--- src/main/java/com/fredson/datastructures/Vector.java
+++ src/main/java/com/fredson/datastructures/Vector.java
@@ -2,98 +2,41 @@ package com.fredson.datastructures;
 
 import java.util.Arrays;
 
-public class Vector<T> {
+public class Vector<T> extends StaticDatastructures<T>{
 
-    private T[] elements;
-    private Integer length;
+    public Vector() {
+        super();
+    }
 
     public Vector(int capacity) {
-        this.elements = (T[]) new Object[capacity];
-        length = 0;
+       super(capacity);
     }
 
     public boolean push(T element){
-        if (isFull()) increaseCapacity();
-        this.elements[length] = element;
-        length ++;
-        return  true;
+        return super.push(element);
     }
 
     public boolean push(T element, int position) {
-        if (isFull()) increaseCapacity();
-        try {
-            if (length + 1 - position >= 0)
-                System.arraycopy(elements, position, elements, position + 1, length + 1 - position);
-            elements[position] = element;
-            length ++;
-        } catch (ArrayIndexOutOfBoundsException error){
-            return false;
-        }
-        return true;
+        return super.push(element, position);
     }
 
     public T getElement(int position) {
-        try {
-            return elements[position];
-        } catch (ArrayIndexOutOfBoundsException error) {
-            return null;
-        }
+        return super.getElement(position);
     }
 
     public int lastIndexOf(T element) {
-        for (int i = length - 1; i >= 0; i --) {
-            if (elements[i].equals(element)) return i;
-        }
-        return - 1;
+        return super.lastIndexOf(element);
     }
 
-    public Boolean contain(T element) {
-        for (Object elementVector : elements) {
-            if (elementVector != null && elementVector.equals(element)) return true;
-        }
-        return false;
+    public boolean contain(T element) {
+        return super.contain(element);
     }
 
-    public Boolean remove(int position) {
-        try {
-            for (int i = position; i <= length - 1; i ++) {
-                elements[i] = elements[i + 1];
-            }
-            length --;
-        } catch (ArrayIndexOutOfBoundsException error) {
-            return false;
-        }
-        return true;
+    public boolean remove(int position) {
+        return super.remove(position);
     }
 
-    public Boolean remove(T element) {
-        for (int i = 0; i <= length; i ++) {
-            if (element.equals(getElement(i))) {
-                remove(i);
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean isEmpty() {
-        return length == 0;
-    }
-
-    private boolean isFull() { return length >= elements.length; }
-
-    private void increaseCapacity() {
-        if (length == elements.length) {
-            T[] newElements = (T[]) new Object[this.elements.length * 2];
-            for (int i = 0; i < length; i ++) {
-                newElements[i] = elements[i];
-            }
-            elements = newElements;
-        }
-    }
-
-    @Override
-    public String toString() {
-        return Arrays.toString(elements);
+    public boolean remove(T element) {
+        return super.remove(element);
     }
 }
