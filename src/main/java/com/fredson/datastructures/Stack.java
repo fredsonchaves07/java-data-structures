47012c4a1aa37d9689fac50d40b38340573e46e6
Fredson Chaves <fredsonchaves07@gmail.com>
2022-03-12 18:04:50 -0300

    WIP: stack datastructure implementation

--- src/main/java/com/fredson/datastructures/Stack.java
+++ src/main/java/com/fredson/datastructures/Stack.java
@@ -1,42 +1,17 @@
 package com.fredson.datastructures;
 
-import java.util.ArrayList;
-import java.util.List;
 
-public class Stack <T>{
+public class Stack <T> extends StaticDatastructures<T>{
     
-    private List<T> items;
-
-    public Stack(){
-        this.items = new ArrayList<>();
+    public Stack() {
+        super();
     }
 
-    public void push(T element){
-        this.items.add(element);
+    public Stack(int capacity) {
+        super(capacity);
     }
 
-    public T pop(){
-        return this.items.remove(this.items.size() - 1);
-    }
-
-    public T peek(){
-        return this.items.get(this.items.size() - 1);
-    }
-
-    public boolean isEmpty(){
-        return this.items.size() == 0;
-    }
-
-    public int size(){
-        return this.items.size();
-    }
-
-    public void clear(){
-        this.items.clear();
-    }
-
-    @Override
-    public String toString() {
-        return this.items.toString();
+    public boolean push(T element) {
+        return super.push(element);
     }
 }
