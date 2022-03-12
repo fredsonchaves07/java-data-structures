a5cd50e4e042fd80e92b42b7b4d1b9c165c543fc
Fredson Chaves <fredsonchaves07@gmail.com>
2022-03-12 18:04:26 -0300

    WIP: stack datastructure implementation

---
+++ src/test/java/com/fredson/datastructures/StackTest.java
@@ -0,0 +1,39 @@
+package com.fredson.datastructures;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class StackTest {
+
+    @Test
+    public void shouldCreateStack() {
+        Stack<String> stack = new Stack<>();
+        assertTrue(stack.push("Java"));
+        assertTrue(stack.push("Python"));
+        assertTrue(stack.push("Javascript"));
+    }
+
+    @Test
+    public void ShouldCreateStackWithCapacity() {
+        Stack<String> stack = new Stack<>(3);
+        assertTrue(stack.push("Java"));
+        assertTrue(stack.push("Python"));
+        assertTrue(stack.push("Javascript"));
+    }
+
+    @Test
+    public void ShouldCreateStackEmpty() {
+        Stack<String> vector = new Stack<>();
+        assertTrue(vector.isEmpty());
+    }
+
+    @Test
+    public void ShouldPrintStack() {
+        Stack<String> stack = new Stack<>();
+        stack.push("Java");
+        stack.push("Python");
+        stack.push("Javascript");
+        System.out.println(stack);
+    }
+}
