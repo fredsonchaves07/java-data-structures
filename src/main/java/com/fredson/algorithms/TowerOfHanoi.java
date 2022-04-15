4d68ecc70a37f579165a4ea3c7b007462d2f59b5
Fredson Chaves <fredsonchaves07@gmail.com>
2022-04-15 12:33:31 -0300

    refact: tower of hanoi algorithm

--- src/main/java/com/fredson/algorithms/TowerOfHanoi.java
+++ src/main/java/com/fredson/algorithms/TowerOfHanoi.java
@@ -6,8 +6,8 @@ import com.fredson.datastructures.Stack;
 
 public class TowerOfHanoi {
 
-    public static void execute(int numberOfDiscs, Stack<Integer> pillarSource, Stack<Integer> pillarDest, Stack<Integer> pillarAux){
-        if(numberOfDiscs == 1){
+    private static void execute(int numberOfDiscs, Stack<Integer> pillarSource, Stack<Integer> pillarDest, Stack<Integer> pillarAux){
+        if (numberOfDiscs == 1){
             pillarDest.push(pillarSource.pop());
         } else {
             execute(numberOfDiscs - 1, pillarSource, pillarAux, pillarDest);
@@ -17,21 +17,15 @@ public class TowerOfHanoi {
     }
     
     public static String towerOfHanoi(int[] discsVector){
-        int numberOfDiscs;
+        int numberOfDiscs = discsVector.length;
         Stack<Integer> pillar1 = new Stack<>();
         Stack<Integer> pillar2 = new Stack<>();
         Stack<Integer> pillar3 = new Stack<>();
         Arrays.sort(discsVector);
-        numberOfDiscs = discsVector.length;
-        for(int disc: discsVector){
+        for (int disc: discsVector){
             pillar1.push(disc);
         }
         execute(numberOfDiscs, pillar1, pillar3, pillar2);
         return "pillar1: " + pillar1 + "\npillar2: " + pillar2 + "\npillar3: " + pillar3;
-    }
-
-    public static void main(String[] args) {
-        int[] discs = {10, 3, 8, 1, 20, 15, 100, 20, 10, 50, 60, 80};
-        System.out.println(towerOfHanoi(discs));
     }
 }
---
+++ src/test/java/com/fredson/algorithms/TowerOfHanoiTest.java
@@ -0,0 +1,16 @@
+package com.fredson.algorithms;
+
+import org.junit.Test;
+
+import static com.fredson.algorithms.TowerOfHanoi.towerOfHanoi;
+import static org.junit.Assert.*;
+
+public class TowerOfHanoiTest {
+
+    @Test
+    public void shouldExecutePalindromeAlgorithm() {
+        int[] discs = {10, 3, 8, 1, 20, 15, 100, 20, 10, 50, 60, 80};
+        assertNotNull(towerOfHanoi(discs));
+        System.out.println(towerOfHanoi(discs));
+    }
+}
