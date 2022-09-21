469b61379c0bacf729993136a30222488278a663
fredsonchaves07 <fredsonchaves07@gmail.com>
2022-09-21 15:31:32 -0300

    feat: finalização da implementação do algoritmo de busca binaria
    Signed-off-by: fredsonchaves07 <fredsonchaves07@gmail.com>

--- src/main/java/com/fredson/datastructures/sequence/Search.java
+++ src/main/java/com/fredson/datastructures/sequence/Search.java
@@ -10,6 +10,28 @@ public class Search {
 
     public static <T extends Comparable<T>> T binarySearch(T[] elements, T element) {
         if (!Search.isSorted(elements)) throw new RuntimeException("List is not ordered");
+        int firstIndex = 0;
+        int lastIndex = elements.length - 1;
+        return binarySearch(elements, element, firstIndex, lastIndex);
+    }
+
+    private static <T extends Comparable<T>> T binarySearch(T[] elements, T element, int firstIndex, int lastIndex) {
+        int lastIndexPos = lastIndex;
+        int firstIndexPos = firstIndex;
+        if (firstIndexPos == elements.length || lastIndex == 0 ) return null;
+        int midleIndex = (lastIndexPos + firstIndexPos) / 2;
+        if ((midleIndex == lastIndex || midleIndex == firstIndex) && firstIndex > lastIndex) {
+            return null;
+        }
+        if (elements[midleIndex].equals(element)) {
+            return element;
+        } else if (elements[midleIndex].compareTo(element) < 0) {
+            firstIndexPos = midleIndex + 1;
+            return binarySearch(elements, element, firstIndexPos, lastIndexPos);
+        } else if (elements[midleIndex].compareTo(element) > 0) {
+            lastIndexPos = midleIndex - 1;
+            return binarySearch(elements, element, firstIndexPos, lastIndexPos);
+        }
         return null;
     }
 
--- src/test/java/com/fredson/datastructures/sequence/SearchTest.java
+++ src/test/java/com/fredson/datastructures/sequence/SearchTest.java
@@ -4,7 +4,6 @@ package com.fredson.datastructures.sequence;
 import org.junit.Test;
 
 import java.util.Arrays;
-import java.util.Collections;
 
 import static org.junit.Assert.assertEquals;
 
@@ -13,9 +12,24 @@ public class SearchTest {
     @Test
     public void shouldGetNamesOnBinarySearch() {
         String[] names = {"Ana", "Carlos", "Beatriz", "Clelton", "Silva", "Maria"};
-        Collections.sort(Arrays.asList(names));
+        Arrays.sort(names);
         assertEquals("Maria", Search.binarySearch(names, "Maria"));
         assertEquals("Beatriz", Search.binarySearch(names, "Beatriz"));
         assertEquals("Ana", Search.binarySearch(names, "Ana"));
+    }
+
+    @Test
+    public void shouldGetNumbersOnBinarySearch() {
+        Integer[] numbers = new Integer[1000];
+        for (int i = 0; i < numbers.length; i ++) {
+            numbers[i] = i;
+        }
+        Arrays.sort(numbers);
+        assertEquals(Integer.valueOf(0), Search.binarySearch(numbers, 0));
+        assertEquals(Integer.valueOf(100), Search.binarySearch(numbers, 100));
+        assertEquals(Integer.valueOf(500), Search.binarySearch(numbers, 500));
+        assertEquals(Integer.valueOf(300), Search.binarySearch(numbers, 300));
+        assertEquals(Integer.valueOf(999), Search.binarySearch(numbers, 999));
+        assertEquals(Integer.valueOf(600), Search.binarySearch(numbers, 600));
     }
 }
