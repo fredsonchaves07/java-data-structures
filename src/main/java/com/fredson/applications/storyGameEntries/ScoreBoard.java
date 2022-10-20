dd2845422c43f7d9e9f4ecd55a0423e6a4d24df4
fredsonchaves07 <fredsonchaves07@gmail.com>
2022-10-20 14:03:11 -0300

    exemplo em aplicacao de array
    Signed-off-by: fredsonchaves07 <fredsonchaves07@gmail.com>

---
+++ src/main/java/com/fredson/applications/storyGameEntries/ScoreBoard.java
@@ -0,0 +1,55 @@
+package com.fredson.applications.storyGameEntries;
+
+import java.util.Arrays;
+
+public class ScoreBoard {
+
+    private int size;
+    private final GameEntry[] board;
+
+    public ScoreBoard(int capacity) {
+        board = new GameEntry[capacity];
+    }
+
+    public void add(GameEntry gameEntry) {
+        if (isMaxCapacity()) throw new IndexOutOfBoundsException("Max GameEntry array capacity");
+        for (int i = 0; i <= size; i ++)
+            if (board[i] == null) board[i] = gameEntry;
+        size ++;
+    }
+
+    public GameEntry get(int index) {
+        if (isInvalidIndex(index)) throw new IndexOutOfBoundsException("Invalid index");
+        return board[index];
+    }
+
+    public void remove(int index) throws IndexOutOfBoundsException {
+        if (isInvalidIndex(index)) throw new IndexOutOfBoundsException("Invalid index");
+        board[index] = null;
+        size --;
+    }
+
+    private boolean isMaxCapacity() {
+        return size >= board.length;
+    }
+
+    private boolean isInvalidIndex(int index) {
+        return index < 0 || index >= board.length;
+    }
+
+    public void sort() {
+        GameEntry[] boardSorted;
+        int minScore;
+        for (int i = 0; i < size; i ++) {
+            if (board[i].getScore() <= board[i + 1])
+        }
+    }
+
+    public int size() {
+        return size;
+    }
+    @Override
+    public String toString() {
+        return Arrays.toString(board);
+    }
+}
---
+++ src/test/java/com/fredson/applications/GameEntryTest.java
@@ -0,0 +1,87 @@
+package com.fredson.applications;
+
+import com.fredson.applications.storyGameEntries.GameEntry;
+import com.fredson.applications.storyGameEntries.ScoreBoard;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class GameEntryTest {
+
+    @Test
+    public void shouldAddGameEntry() {
+        GameEntry gameEntryJoao = new GameEntry("Joao", 100);
+        GameEntry gameEntryMaria = new GameEntry("Maria", 500);
+        GameEntry gameEntryCarlos = new GameEntry("Carlos", 0);
+        GameEntry gameEntryAlice = new GameEntry("Alice", 30);
+        ScoreBoard scoreboard = new ScoreBoard(4);
+        scoreboard.add(gameEntryJoao);
+        scoreboard.add(gameEntryMaria);
+        scoreboard.add(gameEntryCarlos);
+        scoreboard.add(gameEntryAlice);
+        assertTrue(true);
+    }
+
+    @Test
+    public void shouldGetGameEntry() {
+        GameEntry gameEntryJoao = new GameEntry("Joao", 100);
+        GameEntry gameEntryMaria = new GameEntry("Maria", 500);
+        GameEntry gameEntryCarlos = new GameEntry("Carlos", 0);
+        GameEntry gameEntryAlice = new GameEntry("Alice", 30);
+        ScoreBoard scoreboard = new ScoreBoard(4);
+        scoreboard.add(gameEntryJoao);
+        scoreboard.add(gameEntryMaria);
+        scoreboard.add(gameEntryCarlos);
+        scoreboard.add(gameEntryAlice);
+        assertEquals(gameEntryJoao, scoreboard.get(0));
+        assertEquals(gameEntryMaria, scoreboard.get(1));
+        assertEquals(gameEntryCarlos, scoreboard.get(2));
+        assertEquals(gameEntryAlice, scoreboard.get(3));
+    }
+
+    @Test
+    public void shouldRemoveGameEntry() {
+        GameEntry gameEntryJoao = new GameEntry("Joao", 100);
+        GameEntry gameEntryMaria = new GameEntry("Maria", 500);
+        GameEntry gameEntryCarlos = new GameEntry("Carlos", 0);
+        GameEntry gameEntryAlice = new GameEntry("Alice", 30);
+        ScoreBoard scoreboard = new ScoreBoard(4);
+        scoreboard.add(gameEntryJoao);
+        scoreboard.add(gameEntryMaria);
+        scoreboard.add(gameEntryCarlos);
+        scoreboard.add(gameEntryAlice);
+        scoreboard.remove(3);
+        assertEquals(3, scoreboard.size());
+    }
+
+    @Test
+    public void shouldGetScoreBoardSize() {
+        GameEntry gameEntryJoao = new GameEntry("Joao", 100);
+        GameEntry gameEntryMaria = new GameEntry("Maria", 500);
+        GameEntry gameEntryCarlos = new GameEntry("Carlos", 0);
+        GameEntry gameEntryAlice = new GameEntry("Alice", 30);
+        ScoreBoard scoreboard = new ScoreBoard(4);
+        scoreboard.add(gameEntryJoao);
+        scoreboard.add(gameEntryMaria);
+        scoreboard.add(gameEntryCarlos);
+        scoreboard.add(gameEntryAlice);
+        assertEquals(4, scoreboard.size());
+    }
+
+    @Test
+    public void getScoreBoardSortedByScore() {
+        String scoreExpected = "[(Carlos ,0), (Alice ,30), (Joao ,100), (Maria ,500)]";
+        GameEntry gameEntryJoao = new GameEntry("Joao", 100);
+        GameEntry gameEntryMaria = new GameEntry("Maria", 500);
+        GameEntry gameEntryCarlos = new GameEntry("Carlos", 0);
+        GameEntry gameEntryAlice = new GameEntry("Alice", 30);
+        ScoreBoard scoreboard = new ScoreBoard(4);
+        scoreboard.add(gameEntryJoao);
+        scoreboard.add(gameEntryMaria);
+        scoreboard.add(gameEntryCarlos);
+        scoreboard.add(gameEntryAlice);
+        scoreboard.sort();
+        assertEquals(scoreExpected, scoreboard);
+    }
+}
