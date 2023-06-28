d5f76ec8f1a4f713b0ec9ac8ac7e55f514232100
fredsonchaves07 <fredsonchaves07@gmail.com>
2023-06-28 18:17:45 -0300

    feat: push circular list
    Signed-off-by: fredsonchaves07 <fredsonchaves07@gmail.com>

--- src/main/java/com/fredson/datastructures/list/CircularLinkedList.java
+++ src/main/java/com/fredson/datastructures/list/CircularLinkedList.java
@@ -1,83 +1,31 @@
-//package com.fredson.datastructures;
-//
-//import com.fredson.models.DoublyNode;
-//
-//public class CircularLinkedList <T> extends DoublyLinkedList <T>{
-//
-//    public CircularLinkedList() {
-//        super();
-//    }
-//
-//    @Override
-//    public void push(T element) {
-//        DoublyNode<T> doublyNode = new DoublyNode<>(element);
-//        if (isEmpty()) {
-//            headNode = doublyNode;
-//        } else {
-//            DoublyNode<T> currentNode = headNode;
-//            if (currentNode.getNextNode() == null) {
-//                currentNode.setNextNode(doublyNode);
-//                doublyNode.setPrevNode(currentNode);
-//                doublyNode.setNextNode(headNode);
-//            }
-//            while (currentNode.getNextNode() != null) {
-//                if (currentNode.getNextNode().equals(headNode) || currentNode.getNextNode() == null) {
-//                    currentNode.setNextNode(doublyNode);
-//                    doublyNode.setPrevNode(currentNode);
-//                    doublyNode.setNextNode(headNode);
-//                    break;
-//                }
-//                currentNode = (DoublyNode<T>) currentNode.getNextNode();
-//            }
-//        }
-//        tailNode = doublyNode;
-//        length += 1;
-//    }
-//
-////    @Override
-////    public void insert(T element, int index) {
-////        if (index >= 0 && index <= this.count) {
-////            Node node = new Node(element);
-////            Node nodeCurrent = this.head;
-////            if (index == 0) {
-////                if (this.head == null) {
-////                    this.head = node;
-////                    node.setNextNode(this.head);
-////                } else {
-////                    node.setNextNode(nodeCurrent);
-////                    nodeCurrent = this.getElementAt(this.size());
-////                    this.head = node;
-////                    nodeCurrent.setNextNode(this.head);
-////                }
-////            } else {
-////                Node previous = this.getElementAt(index - 1);
-////                node.setNextNode(previous.getNextNode());
-////                previous.setNextNode(node);
-////            }
-////            this.count ++;
-////        }
-////    }
-////
-////    @Override
-////    public void removeAt(int index) {
-////        if (index >= 0 && index < this.count) {
-////            Node nodeCurrent = this.head;
-////            if (index == 0) {
-////                if (this.size() == 1) {
-////                    this.head = null;
-////                } else {
-////                    Node removed = this.head;
-////                    nodeCurrent = this.getElementAt(this.size());
-////                    this.head = this.head.getNextNode();
-////                    nodeCurrent.setNextNode(this.head);
-////                    nodeCurrent = removed;
-////                }
-////            } else {
-////                Node previous = this.getElementAt(index - 1);
-////                nodeCurrent = previous.getNextNode();
-////                previous.setNextNode(nodeCurrent.getNextNode());
-////            }
-////            this.count --;
-////        }
-////    }
-//}
+package com.fredson.datastructures.list;
+
+import com.fredson.models.Node;
+
+import java.util.ArrayList;
+
+public class CircularLinkedList <T> extends LinkedList<T> {
+
+    public CircularLinkedList() {
+        super();
+    }
+
+        @Override
+    public void push(T element) {
+        super.push(element);
+        if (tailNode.getNextNode() != headNode && !tailNode.equals(headNode))
+            tailNode.setNextNode(new Node<>(headNode.getElement()));
+    }
+
+    @Override
+    public String toString() {
+        java.util.List<T> elements = new ArrayList<>();
+        Node<T> currentNode = this.headNode;
+        while (currentNode != null) {
+            elements.add(currentNode.getElement());
+            currentNode = currentNode.getNextNode();
+            if (currentNode!= null && currentNode.equals(headNode)) break;
+        }
+        return elements.toString();
+    }
+}
