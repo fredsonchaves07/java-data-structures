58177a0f8df8a5835c8d955868f76284125a1555
fredsonchaves07 <fredsonchaves07@gmail.com>
2023-07-27 09:48:52 -0300

    refact: Improved code readability
    Signed-off-by: fredsonchaves07 <fredsonchaves07@gmail.com>

--- src/main/java/com/fredson/algorithms/BaseConverter.java
+++ src/main/java/com/fredson/algorithms/BaseConverter.java
@@ -7,15 +7,15 @@ public class BaseConverter {
 
     final static String DIGITS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    public static String baseConverter(int number, int base){
+    public static String baseConverter(int number, int base) {
         Stack<Integer> stack = new ArrayStack<>();
         StringBuilder baseConverterString = new StringBuilder();
-        while (number > 0){
+        while (number > 0) {
             int rem = number % base;
             number = number / base;
             stack.push(rem);
         }
-        while (!stack.isEmpty()){
+        while (!stack.isEmpty()) {
             baseConverterString.append(DIGITS.charAt(stack.pop()));
         }
         return baseConverterString.toString();
