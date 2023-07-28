800b2a7a78e60a5b25fee33f78c9b720c0aaedd5
fredsonchaves07 <fredsonchaves07@gmail.com>
2023-07-28 09:48:54 -0300

    refact: renam variable algorithm
    Signed-off-by: fredsonchaves07 <fredsonchaves07@gmail.com>

--- src/test/java/com/fredson/algorithms/TowerOfHanoiTest.java
+++ src/test/java/com/fredson/algorithms/TowerOfHanoiTest.java
@@ -3,12 +3,12 @@ package com.fredson.algorithms;
 import org.junit.Test;
 
 import static com.fredson.algorithms.TowerOfHanoi.towerOfHanoi;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertNotNull;
 
 public class TowerOfHanoiTest {
 
     @Test
-    public void shouldExecutePalindromeAlgorithm() {
+    public void shouldExecuteTowerOfHanoiAlgorithm() {
         int[] discs = {10, 3, 8, 1, 20, 15, 100, 20, 10, 50, 60, 80};
         assertNotNull(towerOfHanoi(discs));
         System.out.println(towerOfHanoi(discs));
