0f2010016700ecbad1003dbb39179cdec90bfa54
fredsonchaves07 <fredsonchaves07@gmail.com>
2023-10-24 18:35:03 -0300

    feat: implementação de alguns métodos auxiliares para arvore
    Signed-off-by: fredsonchaves07 <fredsonchaves07@gmail.com>

---
+++ src/main/java/com/fredson/datastructures/tree/Tree.java
@@ -0,0 +1,59 @@
+package com.fredson.datastructures.tree;
+
+import com.fredson.datastructures.iterator.Iterator;
+import com.fredson.datastructures.list.ArrayList;
+import com.fredson.datastructures.list.List;
+import com.fredson.models.Node;
+
+public interface Tree<T> {
+
+    T replace(Node<T> node, T element);
+
+    Node<T> root();
+
+    Node<T> parent(Node<T> node);
+
+    List<Node<T>> nodes();
+
+    List<Node<T>> children(Node<T> node);
+
+    boolean isInternal(Node<T> node);
+
+    boolean isExternal(Node<T> node);
+
+    boolean isRoot(Node<T> node);
+
+    int size();
+
+    boolean isEmpty();
+
+    Iterator<T> iterator();
+
+    default int depth(Node<T> node) {
+        if (this.isEmpty()) return 0;
+        return 1 + depth(this.parent(node));
+    }
+
+    default int height(Node<T> node) {
+        if (this.isExternal(node)) return 0;
+        int heigh = 0;
+        while (this.children(node).iterator().hasNext()) {
+            heigh = Math.max(heigh, height(this.children(node).iterator().next()));
+        }
+        return 1 + heigh;
+    }
+
+    default List<T> preOrder(Node<T> node) {
+        List<T> elements = new ArrayList<>();
+        getElementNode(node, elements);
+        return elements;
+    }
+
+    private T getElementNode(Node<T> node, List<T> elements) {
+        T element = node.getElement();
+        while (this.children(node).iterator().hasNext()) {
+            elements.push(getElementNode(this.children(node).iterator().next(), elements));
+        }
+        return element;
+    }
+}
