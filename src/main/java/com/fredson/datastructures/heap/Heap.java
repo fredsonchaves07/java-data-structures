22fdc1ddbc909674e3a152b8379b07566b418477
fredsonchaves07 <fredsonchaves07@gmail.com>
2024-01-17 18:25:42 -0300

    feat: implementação de interface de heap
    Signed-off-by: fredsonchaves07 <fredsonchaves07@gmail.com>

--- src/main/java/com/fredson/datastructures/heap/Heap.java
+++ src/main/java/com/fredson/datastructures/heap/Heap.java
@@ -1,6 +1,7 @@
 package com.fredson.datastructures.heap;
 
 import com.fredson.datastructures.DataStructure;
+import com.fredson.datastructures.list.List;
 
 public interface Heap<T> extends DataStructure<T> {
 
@@ -10,5 +11,18 @@ public interface Heap<T> extends DataStructure<T> {
 
     T find();
 
+    List<T> toList();
 
+    default int getLeftIndex(int index) {
+        return 2 * index + 1;
+    }
+
+    default int getRightIndex(int index) {
+        return 2 * index + 2;
+    }
+
+    default int getParentIndex(int index) {
+        if (index == 0) return 0;
+        return (int) Math.floor(((double) index - 1) / 2);
+    }
 }
--- src/main/java/com/fredson/datastructures/heap/MinHeap.java
+++ src/main/java/com/fredson/datastructures/heap/MinHeap.java
@@ -1,12 +1,48 @@
 package com.fredson.datastructures.heap;
 
-public class MinHeap<T> implements Heap<T>{
+import com.fredson.datastructures.iterator.Iterator;
+import com.fredson.datastructures.list.ArrayList;
+import com.fredson.datastructures.list.List;
 
+
+public class MinHeap<T extends Comparable<T>> implements Heap<T>{
+
+    private final List<T> heap;
+
+    private int length;
+
+    public MinHeap() {
+        heap = new ArrayList<>();
+    }
 
     @Override
     public void insert(T element) {
-
+        if (isEmpty()) {
+            heap.push(element);
+            length ++;
+        } else {
+            heap.push(element);
+            length ++;
+            siftUp(length() - 1);
+        }
     }
+
+    private void siftUp(int index) {
+        T element = heap.getElement(getParentIndex(index));
+        while (index > 0 && element.compareTo(heap.getElement(index)) > 0) {
+            swap(heap.getElement(index), element);
+            index = getParentIndex(index);
+            element = heap.getElement(getParentIndex(index));
+        }
+    }
+
+    private void swap(T newElement, T oldElement) {
+        int newElementIndex = heap.indexOf(newElement);
+        int oldElementIndex = heap.indexOf(oldElement);
+        heap.push(oldElement, newElementIndex);
+        heap.push(newElement, oldElementIndex);
+    }
+
 
     @Override
     public T extract() {
@@ -16,5 +52,58 @@ public class MinHeap<T> implements Heap<T>{
     @Override
     public T find() {
         return null;
+    }
+
+    @Override
+    public List<T> toList() {
+        return heap.clone();
+    }
+
+    @Override
+    public int length() {
+        return length;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return length() == 0;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    public T getMin() {
+        return heap.getElement(0);
+    }
+
+    @Override
+    public String toString() {
+        if (isEmpty()) return "[]";
+        String treeString = getMin().toString();
+        treeString = toString(1, treeString);
+        return treeString;
+    }
+
+    private String toString(int index, String treeString) {
+        while (index  > 0 && index < length()) {
+            treeString += " -> (";
+            treeString += heap.getElement(index);
+            int leftIndex = getLeftIndex(index);
+            if (leftIndex < length()) {
+                treeString = toString(getLeftIndex(index), treeString);
+            }
+            int rightIndex = index + 1;
+            if (rightIndex < length()) {
+                treeString += ", ";
+                treeString += heap.getElement(rightIndex);
+                treeString += ")";
+                return treeString;
+            }  else {
+                break;
+            }
+        }
+        return treeString;
     }
 }
