be687aadaeade9252bec1b9963e31ff2d80ff7a4
fredsonchaves07 <fredsonchaves07@gmail.com>
2024-02-02 18:36:03 -0300

    feat: implementação de testes de heap
    Signed-off-by: fredsonchaves07 <fredsonchaves07@gmail.com>

--- src/main/java/com/fredson/datastructures/heap/MinHeap.java
+++ src/main/java/com/fredson/datastructures/heap/MinHeap.java
@@ -3,60 +3,131 @@ package com.fredson.datastructures.heap;
 import com.fredson.datastructures.iterator.Iterator;
 import com.fredson.datastructures.list.ArrayList;
 import com.fredson.datastructures.list.List;
+import com.fredson.datastructures.tree.BinarySearchTree;
+import com.fredson.datastructures.tree.Tree;
 
 
 public class MinHeap<T extends Comparable<T>> implements Heap<T>{
 
-    private final List<T> heap;
+    private final List<T> heap = new ArrayList<>();
 
     private int length;
 
+    private T minElement;
+
     public MinHeap() {
-        heap = new ArrayList<>();
+    }
+
+    public MinHeap(Tree<T> tree) {
+        List<T> treeList = tree.toList().clone();
+        while (treeList.iterator().hasNext()) {
+            T element = treeList.iterator().next();
+            insert(element);
+        }
     }
 
     @Override
     public void insert(T element) {
         if (isEmpty()) {
             heap.push(element);
+            minElement = element;
             length ++;
         } else {
             heap.push(element);
             length ++;
-            siftUp(length() - 1);
+            siftUp(heap.getElement(element));
         }
     }
 
-    private void siftUp(int index) {
-        T element = heap.getElement(getParentIndex(index));
-        while (index > 0 && element.compareTo(heap.getElement(index)) > 0) {
-            swap(heap.getElement(index), element);
-            index = getParentIndex(index);
-            element = heap.getElement(getParentIndex(index));
+    private void siftUp(T element) {
+        int index = heap.indexOf(element);
+        T elementHeap = heap.getElement(getParentIndex(index));
+        while (index >= 0 && elementHeap != null && elementHeap.compareTo(element) > 0) {
+            swap(element, elementHeap);
+            index = getParentIndex(heap.indexOf(element));
+            elementHeap = heap.getElement(index);
+            while (elementHeap == null && index == 0) {
+                index ++;
+                elementHeap = heap.getElement(index);
+                if (elementHeap != null) {
+                    swap(element, elementHeap);
+                    index = 0;
+                }
+            }
+        }
+    }
+
+    private void siftDown(int index) {
+        int elementIndex = index;
+        if (index == 0) {
+            swap(heap.getElement(length() - 1), minElement);
+            heap.remove(minElement);
+        }
+        int leftIndex = getLeftIndex(index);
+        int rigthIndex = getRightIndex(index);
+        T leftElement = null;
+        if (leftIndex < length()) {
+            leftElement = heap.getElement(leftIndex);
+        }
+        T rigthElement = null;
+        if (rigthIndex < length()) {
+            rigthElement = heap.getElement(rigthIndex);
+        }
+        T element = heap.getElement(index);
+        if (leftElement != null && leftElement.compareTo(element) < 0) {
+            swap(leftElement, element);
+            elementIndex = leftIndex;
+        } else if (rigthElement != null && rigthElement.compareTo(element) < 0) {
+            swap(leftElement, element);
+            elementIndex = rigthIndex;
+        }
+        if (index != elementIndex) {
+            siftDown(elementIndex);
         }
     }
 
     private void swap(T newElement, T oldElement) {
         int newElementIndex = heap.indexOf(newElement);
-        int oldElementIndex = heap.indexOf(oldElement);
+        int oldElementIndex;
+        if (oldElement.equals(minElement)) {
+            oldElementIndex = 0;
+        } else {
+            oldElementIndex = heap.indexOf(oldElement);
+        }
         heap.push(oldElement, newElementIndex);
         heap.push(newElement, oldElementIndex);
+        if (!minElement.equals(newElement) && minElement.compareTo(newElement) > 0)
+            minElement = newElement;
     }
-
 
     @Override
     public T extract() {
-        return null;
+        if (isEmpty()) return null;
+        T element = minElement;
+        if (length() != 1) {
+            siftDown(0);
+        } else {
+            heap.remove(minElement);
+        }
+        minElement = heap.getElement(0);
+        length --;
+        return element;
     }
 
     @Override
     public T find() {
-        return null;
+        if (isEmpty()) return null;
+        return minElement;
     }
 
     @Override
     public List<T> toList() {
         return heap.clone();
+    }
+
+    @Override
+    public Tree<T> toTree() {
+        return new BinarySearchTree<>(this);
     }
 
     @Override
@@ -74,35 +145,34 @@ public class MinHeap<T extends Comparable<T>> implements Heap<T>{
         return null;
     }
 
-    public T getMin() {
-        return heap.getElement(0);
-    }
-
     @Override
     public String toString() {
         if (isEmpty()) return "[]";
-        String treeString = getMin().toString();
-        treeString = toString(1, treeString);
+        String treeString = minElement.toString();
+        treeString = toString(0, treeString);
         return treeString;
     }
 
     private String toString(int index, String treeString) {
-        while (index  > 0 && index < length()) {
+        int leftIndex = getLeftIndex(index);
+        int rightIndex = getRightIndex(index);
+        T leftElement = null;
+        T rigthElement = null;
+        if (leftIndex <= length()) leftElement = heap.getElement(leftIndex);
+        if (rightIndex <= length()) rigthElement = heap.getElement(rightIndex);
+        if (leftElement != null) {
             treeString += " -> (";
-            treeString += heap.getElement(index);
-            int leftIndex = getLeftIndex(index);
-            if (leftIndex < length()) {
-                treeString = toString(getLeftIndex(index), treeString);
-            }
-            int rightIndex = index + 1;
-            if (rightIndex < length()) {
-                treeString += ", ";
-                treeString += heap.getElement(rightIndex);
+            treeString += leftElement;
+            treeString = toString(leftIndex, treeString);
+            if (rigthElement == null) {
                 treeString += ")";
-                return treeString;
-            }  else {
-                break;
             }
+        }
+        if (rigthElement != null) {
+            treeString += ", ";
+            treeString += rigthElement;
+            treeString = toString(rightIndex, treeString);
+            treeString += ")";
         }
         return treeString;
     }
--- src/test/java/com/fredson/datastructures/heap/HeapTest.java
+++ src/test/java/com/fredson/datastructures/heap/HeapTest.java
@@ -1,9 +1,10 @@
 package com.fredson.datastructures.heap;
 
+import com.fredson.datastructures.tree.LinkedBinaryTree;
+import com.fredson.datastructures.tree.Tree;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 public class HeapTest {
 
@@ -21,5 +22,108 @@ public class HeapTest {
         assertEquals(5, heap.length());
         assertEquals(heapListExpected, heap.toList().toString());
         assertEquals(heapStringExpected, heap.toString());
+    }
+
+    @Test
+    public void shouldReturnMinHeapIsEmpty() {
+        Heap<Integer> heap = new MinHeap<>();
+        assertTrue(heap.isEmpty());
+        assertEquals(0, heap.length());
+        assertEquals("[]", heap.toList().toString());
+        assertEquals("[]", heap.toString());
+    }
+
+    @Test
+    public void shouldReturnMinHeapWithBinaryTree() {
+        Tree<String> tree = new LinkedBinaryTree<>();
+        String heapListExpected = "[Academico, Contas a pagar, Empresa A, Financeiro, Contas a receber, Secretaria, Tesouraria]";
+        String heapStringExpected = "Academico -> (Contas a pagar -> (Financeiro, Contas a receber), Empresa A -> (Secretaria, Tesouraria))";
+        tree.insert("Empresa A");
+        tree.insert("Financeiro");
+        tree.insert("Academico");
+        tree.insert("Financeiro", "Contas a pagar");
+        tree.insert("Financeiro", "Contas a receber");
+        tree.insert("Academico", "Secretaria");
+        tree.insert("Contas a receber", "Tesouraria");
+        Heap<String> heap = new MinHeap<>(tree);
+        assertFalse(heap.isEmpty());
+        assertEquals(7, heap.length());
+        assertEquals(heapListExpected, heap.toList().toString());
+        assertEquals(heapStringExpected, heap.toString());
+    }
+
+    @Test
+    public void shouldFindMinValueInHeap() {
+        Heap<Integer> heap = new MinHeap<>();
+        heap.insert(2);
+        heap.insert(3);
+        heap.insert(4);
+        heap.insert(5);
+        heap.insert(1);
+        assertEquals(Integer.valueOf(1), heap.find());
+    }
+
+    @Test
+    public void shouldExtractValueMinInHeap() {
+        Heap<Integer> heap = new MinHeap<>();
+        String heapListExpected = "[2, 3, 4, 5]";
+        String heapStringExpected = "2 -> (3 -> (5), 4)";
+        heap.insert(2);
+        heap.insert(3);
+        heap.insert(4);
+        heap.insert(5);
+        heap.insert(1);
+        assertEquals(Integer.valueOf(1), heap.extract());;
+        assertFalse(heap.isEmpty());
+        assertEquals(4, heap.length());
+        assertEquals(heapListExpected, heap.toList().toString());
+        assertEquals(heapStringExpected, heap.toString());
+    }
+
+    @Test
+    public void shouldExtractValueMinInHeapWithOneElement() {
+        Heap<Integer> heap = new MinHeap<>();
+        String heapStringExpected = "[]";
+        heap.insert(2);
+        assertEquals(Integer.valueOf(2), heap.extract());;
+        assertTrue(heap.isEmpty());
+        assertTrue(heap.toList().isEmpty());
+        assertEquals(heapStringExpected, heap.toString());
+    }
+
+    @Test
+    public void shouldInsertElementWithExtractValue() {
+        Heap<Integer> heap = new MinHeap<>();
+        String heapListExpected = "[0, 3, 2, 5, 8, 4, 6]";
+        String heapStringExpected = "0 -> (3 -> (5, 8), 2 -> (4, 6))";
+        heap.insert(2);
+        heap.insert(3);
+        heap.insert(4);
+        heap.insert(5);
+        heap.insert(1);
+        assertEquals(Integer.valueOf(1), heap.extract());;
+        assertFalse(heap.isEmpty());
+        assertEquals(4, heap.length());
+        heap.insert(8);
+        heap.insert(0);
+        heap.insert(6);
+        assertEquals(Integer.valueOf(0), heap.find());
+        assertEquals(7, heap.length());
+        assertEquals(heapListExpected, heap.toList().toString());
+        assertEquals(heapStringExpected, heap.toString());
+    }
+
+    @Test
+    public void shouldConvertHeapInTree() {
+        Heap<Integer> heap = new MinHeap<>();
+        String treeStringExpected = "0 -> (3 -> (5 -> (8 -> (6), 4), 2))";
+        heap.insert(2);
+        heap.insert(3);
+        heap.insert(4);
+        heap.insert(5);
+        heap.insert(8);
+        heap.insert(0);
+        heap.insert(6);
+        assertEquals(treeStringExpected, heap.toTree().toString());
     }
 }
