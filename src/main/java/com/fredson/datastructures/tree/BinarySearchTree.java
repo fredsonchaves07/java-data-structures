a21f3a5cdc3f234bf86b501367597c217f5d6563
fredsonchaves07 <fredsonchaves07@gmail.com>
2024-02-07 10:47:53 -0300

    feat: alterando ordem de valor mínimo e maior
    Signed-off-by: fredsonchaves07 <fredsonchaves07@gmail.com>

--- src/main/java/com/fredson/datastructures/tree/BinarySearchTree.java
+++ src/main/java/com/fredson/datastructures/tree/BinarySearchTree.java
@@ -22,15 +22,6 @@ public class BinarySearchTree<T extends Comparable<T>> extends LinkedBinaryTree<
         DoublyNode<T> node = nodes.get(nodeElement);
         if (node == null) throw new Error("Node " + nodeElement + " not found in tree");
         if (nodeElement.compareTo(element) < 0 || nodeElement.compareTo(element) == 0) {
-            if (node.getPrevNode() == null) {
-                DoublyNode<T> newNode = new DoublyNode<>(element);
-                node.setPrevNode(newNode);
-                nodes.put(newNode.getElement(), newNode);
-                size ++;
-            } else {
-                insert(node.getPrevNode().getElement(), element);
-            }
-        } else if (nodeElement.compareTo(element) > 0) {
             if (node.getNextNode() == null) {
                 DoublyNode<T> newNode = new DoublyNode<>(element);
                 node.setNextNode(newNode);
@@ -38,6 +29,15 @@ public class BinarySearchTree<T extends Comparable<T>> extends LinkedBinaryTree<
                 size ++;
             } else {
                 insert(node.getNextNode().getElement(), element);
+            }
+        } else if (nodeElement.compareTo(element) > 0) {
+            if (node.getPrevNode() == null) {
+                DoublyNode<T> newNode = new DoublyNode<>(element);
+                node.setPrevNode(newNode);
+                nodes.put(newNode.getElement(), newNode);
+                size ++;
+            } else {
+                insert(node.getPrevNode().getElement(), element);
             }
         }
     }
